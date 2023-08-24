0eb2e2174563c748436c63c5fbfd0ceaba6b9753
Egemen <egemenuysalll@gmail.com>
2023-08-24 21:11:37 +0300

    javascript kodları düzeltildi

--- javascript.js
+++ javascript.js
@@ -1,11 +1,17 @@
-function arttir(){
+function arttir() {
+    var sonuc = document.getElementById("sonuc");
+    var currentValue = Number(sonuc.value);
     
-    var sonuc=document.getElementById("sonuc");
-    sonuc.value=Number(sonuc.value)+1;
+    if (currentValue >= 0) {
+        sonuc.value = currentValue + 1;
+    }
 }
      
-    function azalt(){
+function azalt() {
+    var sonuc = document.getElementById("sonuc");
+    var currentValue = Number(sonuc.value);
     
-    var sonuc=document.getElementById("sonuc");
-    sonuc.value=Number(sonuc.value)-1;
+    if (currentValue > 0) {
+        sonuc.value = currentValue - 1;
+    }
 }
