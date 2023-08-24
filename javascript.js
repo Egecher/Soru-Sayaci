e5c6c24274236cf6d5f46402c32ae9e72ee7c233
Egemen <egemenuysalll@gmail.com>
2023-08-24 17:17:38 +0300

    proje eklendi

---
+++ javascript.js
@@ -0,0 +1,11 @@
+function arttir(){
+ 
+    var sonuc=document.getElementById("sonuc");
+    sonuc.value=Number(sonuc.value)+1;
+}
+     
+    function azalt(){
+     
+    var sonuc=document.getElementById("sonuc");
+    sonuc.value=Number(sonuc.value)-1;
+}
