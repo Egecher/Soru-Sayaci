bfc197743bd6c5f464a12926f820a23a0f322639
Egecher <egemenuysalll@gmail.com>
2025-10-02 22:15:34 +0300

    refactor: delete space

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 function arttir() {
     var sonuc = document.getElementById("sonuc");
     var currentValue = Number(sonuc.value);
-    
+
     if (currentValue >= 0) {
         sonuc.value = currentValue + 1;
     }
 }
-     
+
 function azalt() {
     var sonuc = document.getElementById("sonuc");
     var currentValue = Number(sonuc.value);
-    
+
     if (currentValue > 0) {
         sonuc.value = currentValue - 1;
     }
